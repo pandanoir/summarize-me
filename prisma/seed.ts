909751ff38ab2e746998a5e8ad60bc01445e2515
pandanoir <moon_in_france@yahoo.co.jp>
2022-07-05 23:36:36 +0900

    Update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,7 +6,15 @@ const main = async () => {
       { title: 'サッカー' },
       { title: 'じゃんけん' },
       { title: '浦島太郎' },
-      { title: 'ツイッター' },
+      { title: '桃太郎' },
+      { title: 'かぐや姫' },
+      { title: '鬼滅の刃' },
+      { title: 'HUNTER×HUNTER' },
+      { title: 'ドラゴンボール' },
+      { title: 'Twitter' },
+      { title: 'Facebook' },
+      { title: 'YouTube' },
+      { title: 'スマートフォン' },
     ],
   });
   await prisma.answer.createMany({
@@ -14,9 +22,9 @@ const main = async () => {
       {
         content:
           'プレイヤーが複数人いて勝敗を決めるゲーム。各プレイヤーはグーチョキパーのいずれかを出す。グーはチョキに、チョキはパーに、パーはグーに勝つ。各プレイヤーが「勝ち」と「負け」に分かれなければ引き分けとなる。',
-        challengeId: (
-          await prisma.challenge.findFirst({ where: { title: 'じゃんけん' } })
-        )?.id!,
+        challengeId: (await prisma.challenge.findFirst({
+          where: { title: 'じゃんけん' },
+        }))!.id,
       },
     ],
   });
@@ -24,36 +32,41 @@ const main = async () => {
     data: [
       {
         userId: '',
-        answerId: (
-          await prisma.answer.findFirst({
-            where: {
-              content:
-                'プレイヤーが複数人いて勝敗を決めるゲーム。各プレイヤーはグーチョキパーのいずれかを出す。グーはチョキに、チョキはパーに、パーはグーに勝つ。各プレイヤーが「勝ち」と「負け」に分かれなければ引き分けとなる。',
-            },
-          })
-        )?.id!,
+        answerId: (await prisma.answer.findFirst({
+          where: {
+            content:
+              'プレイヤーが複数人いて勝敗を決めるゲーム。各プレイヤーはグーチョキパーのいずれかを出す。グーはチョキに、チョキはパーに、パーはグーに勝つ。各プレイヤーが「勝ち」と「負け」に分かれなければ引き分けとなる。',
+          },
+        }))!.id,
       },
     ],
   });
-  const 浦島太郎 = await prisma.challenge.findFirst({
-    where: {
-      title: '浦島太郎',
-    },
-  });
 
-  if (浦島太郎)
-    await prisma.label.createMany({
-      data: [
-        {
-          challengeId: 浦島太郎.id,
-          name: 'チュートリアル',
-        },
-        {
-          challengeId: 浦島太郎.id,
-          name: '物語',
-        },
-      ],
-    });
+  await prisma.label.createMany({
+    data: [
+      ...[
+        (await prisma.challenge.findFirst({ where: { title: '浦島太郎' } }))!,
+        (await prisma.challenge.findFirst({ where: { title: 'かぐや姫' } }))!,
+        (await prisma.challenge.findFirst({ where: { title: '桃太郎' } }))!,
+      ].flatMap(({ id }) => [
+        { challengeId: id, name: 'チュートリアル' },
+        { challengeId: id, name: '物語' },
+        { challengeId: id, name: '昔話' },
+      ]),
+      ...[
+        (await prisma.challenge.findFirst({
+          where: { title: 'ドラゴンボール' },
+        }))!,
+        (await prisma.challenge.findFirst({
+          where: { title: 'HUNTER×HUNTER' },
+        }))!,
+        (await prisma.challenge.findFirst({ where: { title: '鬼滅の刃' } }))!,
+      ].flatMap(({ id }) => [
+        { challengeId: id, name: '物語' },
+        { challengeId: id, name: '漫画' },
+      ]),
+    ],
+  });
 };
 
 main()
